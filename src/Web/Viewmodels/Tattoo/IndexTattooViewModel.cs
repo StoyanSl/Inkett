5a83db2cc075521e71bec2af5c0c8d6ea7c0fddf
StoyanSl <Stoyansl29@gmail.com>
2019-01-04 00:46:55 +0200

    Tattoo Review Implemented

---
+++ src/Web/Viewmodels/Tattoo/IndexTattooViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Inkett.Web.Viewmodels.Profile;
+using System.Collections.Generic;
+
+namespace Inkett.Web.Viewmodels.Tattoo
+{
+    public class IndexTattooViewModel
+    {
+        public IndexTattooViewModel()
+        {
+            this.StylesViewModels = new List<StyleViewModel>();
+        }
+        public ProfileViewModel ProfileViewModel { get; set; }
+
+        public string Description { get; set; }
+
+        public List<StyleViewModel> StylesViewModels { get; set; }
+
+        public string PictureUri { get; set; }
+    }
+}
---
+++ src/Web/Viewmodels/Tattoo/StyleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inkett.Web.Viewmodels.Tattoo
+{
+    public class StyleViewModel
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+    }
+}
